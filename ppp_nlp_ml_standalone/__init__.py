acf21f2d10c9b39cdcdf424eec5dc6563e0c73e6
Valentin Lorentz <progval@progval.net>
2014-11-10 16:00:19 +0100

    Add missing file.

---
+++ ppp_nlp_ml_standalone/__init__.py
@@ -0,0 +1,9 @@
+"""Example python module for the PPP."""
+
+from ppp_core import HttpRequestHandler
+from .requesthandler import RequestHandler
+
+def app(environ, start_response):
+    """Function called by the WSGI server."""
+    return HttpRequestHandler(environ, start_response, RequestHandler) \
+            .dispatch()

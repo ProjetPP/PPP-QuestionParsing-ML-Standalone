c78ade7a8e6272f3a0e9bc20dc6660babc1fd568
Valentin Lorentz <progval@progval.net>
2014-11-19 22:13:41 +0100

    ???

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 DATA=`/usr/bin/env python3 -c "print(__import__('ppp_nlp_ml_standalone.config').config.get_data('embeddings-scaled.EMBEDDING_SIZE=25.txt.gz'))"`
 wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz;
 gzip -d embeddings-scaled.EMBEDDING_SIZE=25.txt.gz
-mv embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA
+mv -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA
 PYTHONPATH=$PWD ./demo/Dataset.py
 PYTHONPATH=$PWD ./demo/Learn.py

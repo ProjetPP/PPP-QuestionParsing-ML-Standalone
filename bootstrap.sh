c9f22d6375c5aade4a72e06973d3832793d673d5
Valentin Lorentz <progval@progval.net>
2014-11-19 19:40:53 +0100

    Single script for bootstrapping everything + make demo files executable.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,6 @@
 cd data;
 wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz;
 gzip -d embeddings-scaled.EMBEDDING_SIZE=25.txt.gz;
-
+cd ..
+PYTHONPATH=$PWD ./demo/Dataset.py
+PYTHONPATH=$PWD ./demo/Learn.py

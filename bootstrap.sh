af72fe11f53bdd80f5af7ac45c23d949e4c50a82
Quentin Cormier <quentincormier@gmail.com>
2014-11-19 23:51:05 +0100

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ DATASET=`/usr/bin/env python3 -c "print(__import__('ppp_nlp_ml_standalone.config
 
 wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz;
 gzip -d embeddings-scaled.EMBEDDING_SIZE=25.txt.gz
-mv -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATASET
-cp data/AnnotatedQuestions.txt $DATA
+mv -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA
+cp -v data/AnnotatedQuestions.txt $DATASET
 PYTHONPATH=$PWD ./demo/Dataset.py
 PYTHONPATH=$PWD ./demo/Learn.py

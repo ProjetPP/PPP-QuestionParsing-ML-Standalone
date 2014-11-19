707b469af101d7d521b3450dddd9661d71213fbb
Valentin Lorentz <progval@progval.net>
2014-11-19 22:44:51 +0100

    typo.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DATA=`/usr/bin/env python3 -c "print(__import__('ppp_nlp_ml_standalone.config').config.get_data('embeddings-scaled.EMBEDDING_SIZE=25.txt.gz'))"`
+DATA=`/usr/bin/env python3 -c "print(__import__('ppp_nlp_ml_standalone.config').config.get_data('embeddings-scaled.EMBEDDING_SIZE=25.txt'))"`
 wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz;
 gzip -d embeddings-scaled.EMBEDDING_SIZE=25.txt.gz
 mv -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA

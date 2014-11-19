13ac8047cca71e9bb3a95412a760be80926ca886
Valentin Lorentz <progval@progval.net>
2014-11-19 20:20:10 +0100

    Obviously…

--- ppp_nlp_ml_standalone/config.py
+++ ppp_nlp_ml_standalone/config.py
@@ -1,8 +1,12 @@
 """Configuration module."""
 
+import os
 from ppp_libmodule.config import Config as BaseConfig
 
 class Config(BaseConfig):
     config_path_variable = 'PPP_NLP_STANDALONE_CONFIG'
     def parse_config(self, data):
         self.data_dir = data['data_dir']
+
+def get_data(filename):
+    return os.path.join(Config().data_dir, filename)

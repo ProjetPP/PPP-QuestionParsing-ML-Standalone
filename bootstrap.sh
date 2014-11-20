3e3dc7384aa22c33bac5c78418f4ef7165b91432
Valentin Lorentz <progval@progval.net>
2014-11-20 08:02:53 +0100

    Remove duplicates.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-DATA=`/usr/bin/env python3 -c "print(__import__('ppp_nlp_ml_standalone.config').config.get_data('embeddings-scaled.EMBEDDING_SIZE=25.txt'))"`
-DATASET=`/usr/bin/env python3 -c "print(__import__('ppp_nlp_ml_standalone.config').config.get_data('AnnotatedQuestions.txt'))"`
+DATA_DIR=`/usr/bin/env python3 -c "print(__import__('ppp_nlp_ml_standalone.config').config.Config().data_path)"`
 
 wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz;
 gzip -d embeddings-scaled.EMBEDDING_SIZE=25.txt.gz
-mv -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA
-cp -v data/AnnotatedQuestions.txt $DATASET
+mv -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA_DIR
+cp -v data/AnnotatedQuestions.txt $DATA_DIR
 PYTHONPATH=$PWD ./demo/Dataset.py
 PYTHONPATH=$PWD ./demo/Learn.py

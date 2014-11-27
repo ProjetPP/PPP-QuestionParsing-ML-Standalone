dd96b8be44604d4390a3880398355d2f0e4a57dd
Valentin Lorentz <progval@progval.net>
2014-11-27 17:03:53 +0100

    Do not re-download the dataset if we already have it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,13 @@
 
 DATA_DIR=`/usr/bin/env python3 -c "print(__import__('ppp_questionparsing_ml_standalone.config').config.Config().data_dir)"`
 
-wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz;
-gzip -d embeddings-scaled.EMBEDDING_SIZE=25.txt.gz
+mkdir -p $DATA_DIR
+
+if [ ! -f embeddings-scaled.EMBEDDING_SIZE=25.txt ]
+then
+    wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz
+    gzip -d embeddings-scaled.EMBEDDING_SIZE=25.txt.gz
+fi
 mv -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA_DIR
 cp -v data/AnnotatedQuestions.txt $DATA_DIR
 PYTHONPATH=$PWD ./demo/Dataset.py

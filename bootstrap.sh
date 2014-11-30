716a79f3802396aea189ab30ade422ae3b3646b6
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-11-30 10:47:47 +0100

    Fix bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+export PPP_ML_STANDALONE_CONFIG=config.json
 DATA_DIR=`/usr/bin/env python3 -c "print(__import__('ppp_questionparsing_ml_standalone.config').config.Config().data_dir)"`
 
 mkdir -p $DATA_DIR
 
 if [ ! -f $DATA_DIR/embeddings-scaled.EMBEDDING_SIZE=25.txt ]
 then
-    wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz -c
+    if [ ! -f embeddings-scaled.EMBEDDING_SIZE=25.txt.gz ]
+    then
+        wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz -c
+    fi
     gzip -d embeddings-scaled.EMBEDDING_SIZE=25.txt.gz
     mv -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA_DIR
 fi

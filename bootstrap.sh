5ca1e142589417c15f1572bc52913916dd91ade1
Valentin Lorentz <progval@progval.net>
2014-11-30 11:10:08 +0100

    Improvements to bootstrap.sh. Closes GH-8.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export PPP_ML_STANDALONE_CONFIG=config.json
 DATA_DIR=`/usr/bin/env python3 -c "print(__import__('ppp_questionparsing_ml_standalone.config').config.Config().data_dir)"`
 
 mkdir -p $DATA_DIR
@@ -8,6 +9,6 @@ if [ ! -f $DATA_DIR/embeddings-scaled.EMBEDDING_SIZE=25.txt ]
 then
     wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=25.txt.gz -c
     gzip -d embeddings-scaled.EMBEDDING_SIZE=25.txt.gz
-    mv -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA_DIR
+    cp -v embeddings-scaled.EMBEDDING_SIZE=25.txt $DATA_DIR
 fi
 python3 -m ppp_questionparsing_ml_standalone bootstrap
